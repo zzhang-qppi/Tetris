f205f6f9d73992a5933874557af555faae620146
Zhiyu Zhang <zzhang.qppi@gmail.com>
2022-10-14 19:44:21 -0500

    left right and soft drop

--- tetris.py
+++ tetris.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from tetrominoes import Tetromino
 
+pygame.init()
 screen = pygame.display.set_mode((322, 642))
 board = pygame.image.load("Background322642.png").convert()
 t = Tetromino("T")
@@ -9,20 +10,47 @@ t.load_image("tetromino_t.png")
 
 screen.blit(board, (0, 0))
 screen.blit(t.img, t.rect)
-speed = (0, 32)
 
+rate = 800
 pygame.display.flip()
 
-while True:
+going = True
+time = pygame.time.get_ticks()
+
+pygame.key.set_repeat()
+
+while going:
+    drop_speed = (0, 32)
+    right_speed = (32, 0)
+    left_speed = (-32, 0)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            going = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            if t.rect.right < 300:
+                t.move(right_speed, screen, screen.blit(board, (0, 0)))
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            if t.rect.left > 20:
+                t.move(left_speed, screen, screen.blit(board, (0, 0)))
+        # elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+        #     if t.rect.bottom < 640:
+        #         t.move(drop_speed, screen, screen.blit(board, (0, 0)))
+        while pygame.key.get_pressed()[pygame.K_DOWN]:
+            t.move(drop_speed, screen, screen.blit(board, (0, 0)))
+            pygame.time.delay(100)
+            if pygame.key.get_pressed()[pygame.K_DOWN] is False:
+                break
 
     if t.rect.bottom > 640:
-        speed = (0, 0)
-    t.rect = t.rect.move(speed)
-    screen.blit(board, (0, 0))
-    screen.blit(t.img, t.rect)
-    pygame.display.flip()
-    pygame.time.delay(50)
+        drop_speed = (0, 0)
+        right_speed = (0, 0)
+        left_speed = (0, 0)
+
+    if pygame.time.get_ticks() - time >= rate:
+        t.move(drop_speed, screen, screen.blit(board, (0, 0)))
+        time = pygame.time.get_ticks()
+
+    # rate decreases by time
+sys.exit()
 
--- tetrominoes.py
+++ tetrominoes.py
@@ -10,10 +10,13 @@ class Tetromino(object):
     def load_image(self, path, size=(90, 60), l_topleft=(1, 1)):
         self.img = pygame.image.load(path).convert_alpha()
         pygame.transform.scale(self.img, size)
-        self.rect = self.rect.get_rect()
+        self.rect = self.img.get_rect()
         self.rect.topleft = l_topleft
 
-    def drop(self, speed, screen, refrate = 1000):
-        self.rect = self.rect.move(speed)
-        screen.
+    def move(self, speed, screen, refresh, refrate=1000):
+        if self.rect.bottom < 640:
+            self.rect = self.rect.move(speed)
+        refresh
+        screen.blit(self.img, self.rect)
+        pygame.display.flip()
 
